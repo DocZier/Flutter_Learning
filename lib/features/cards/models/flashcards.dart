d64908ec792b475ace3fe6631cb348379ff7dcda
DocZier <doczier@gmail.com>
2025-10-16 20:29:28 +0300

    Added falshcrad model with algorithm of interval repetition SM-2

---
+++ lib/features/cards/models/flashcards.dart
@@ -0,0 +1,32 @@
+class Flashcard {
+  final String id;
+  String question;
+  String answer;
+  //Интервал измеряется в днях
+  int interval;
+  DateTime nextReview;
+  double easeFactor;
+
+  Flashcard({
+    required this.id,
+    required this.question,
+    required this.answer,
+    required this.interval,
+    required this.nextReview,
+    required this.easeFactor,
+  });
+
+  // Алгоритм интервального повторения SM-2
+  void updateCard(int quality) {
+    if (quality <= 3) {
+      interval = 1;
+    } else {
+      interval = (interval * easeFactor).round();
+    }
+
+    easeFactor += (0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02));
+    if (easeFactor < 1.3) easeFactor = 1.3;
+
+    nextReview = DateTime.now().add(Duration(days: interval));
+  }
+}
