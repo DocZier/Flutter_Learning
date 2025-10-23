5ad00c8527a437af10174eec695ec200fa7a90ea
DocZier <doczier@gmail.com>
2025-10-23 23:55:15 +0300

    Added new screen with images of cute snakes/ Thissss issss jusssst tesssst sssscreen. Very cute snakes UwU.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_practic/state/flashcard_container.dart';
+import 'package:test_practic/features/image_loader/screen/test_imagecached_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const FlashcardContainer(),
+      home: const TestImageListScreen(),
     );
   }
 }
